06a6cf090354c6037a85b14f1945427b35254733
Александр Яцевский <sashayatsevskiy@ukr.net>
2020-08-28 17:46:04 +0300

    Bug fixes

---
+++ client/src/app/register-page/register-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent implements OnInit {
+
+  form : FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      email : new FormControl(null,
+        [Validators.required, Validators.email]),
+      password : new FormControl(null,
+        [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit(){}
+
+}
